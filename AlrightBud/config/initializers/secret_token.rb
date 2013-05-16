58efcd01d8849325fcbd869ee73415637aec99b8
Karaignee <karen_rooney@hotmail.com>
2013-05-16 16:22:45 +0100

    rails new

---
+++ AlrightBud/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlrightBud::Application.config.secret_token = '97fa315eb54536e42c4558f2d69a5df06fbd7537194f9b82fc0354e6f2ed1751a5fc06e13122e1b6df2b6a0375438af55ae1f43c90a31008c52de4665ee14136'
